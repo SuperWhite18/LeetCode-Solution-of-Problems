35f8ee47b96d1b1e4808cb46a4d02fa8e155b3b7
superbaiao1208 <1299478311@qq.com>
2021-04-15 22:42:16 +0800

    动态规划

---
+++ LeetCode_April_clock_in/4_15.213.打家劫舍Ⅱ.cpp
@@ -0,0 +1,27 @@
+//题目链接：
+
+https://leetcode-cn.com/problems/house-robber-ii/
+//代码实现：
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if (nums.size() == 0) return 0;
+        if (nums.size() == 1) return nums[0];
+        int result1 = robRange(nums, 0, nums.size() - 2); // 情况二
+        int result2 = robRange(nums, 1, nums.size() - 1); // 情况三
+        return max(result1, result2);
+    }
+    
+    int robRange(vector<int>& nums, int start, int end) {
+        if (end == start) return nums[start];
+        vector<int> dp(nums.size());
+        dp[start] = nums[start];
+        dp[start + 1] = max(nums[start], nums[start + 1]);
+        for (int i = start + 2; i <= end; i++) {
+            dp[i] = max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+        return dp[end];
+    }
+};
+
