55723e4cd352018653426b8299e1fd5cc0ade09e
superbaiao1208 <1299478311@qq.com>
2021-04-18 22:56:28 +0800

    双指针

---
+++ LeetCode_April_clock_in/4_18.26.删除有序数组中的重复项.cpp
@@ -0,0 +1,17 @@
+//题目链接：
+https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array/
+
+//代码实现：
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.empty()) return 0; // 别忘记空数组的判断
+        int slowIndex = 0;
+        for (int fastIndex = 0; fastIndex < (nums.size() - 1); fastIndex++){
+            if(nums[fastIndex] != nums[fastIndex + 1]) { // 发现和后一个不相同
+                nums[++slowIndex] = nums[fastIndex + 1]; //slowIndex = 0 的数据一定是不重复的，所以直接 ++slowIndex
+            }
+        }
+        return slowIndex + 1; //别忘了slowIndex是从0开始的，所以返回slowIndex + 1
+    }
+};
