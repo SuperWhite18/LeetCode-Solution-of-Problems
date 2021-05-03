00ef5dbc07afcb8af9da518d57be61df95ebb223
superbaiao1208 <1299478311@qq.com>
2021-05-03 23:27:37 +0800

    模拟

---
+++ LeetCode_May_clock_in/5_3_7.整数反转.cpp
@@ -0,0 +1,19 @@
+//题目链接：
+https://leetcode-cn.com/problems/reverse-integer/
+
+//代码实现：
+class Solution {
+public:
+    int reverse(int x) 
+    {
+        int r = 0;
+        while(x)
+        {
+            if(r > 0 && r > (INT_MAX - x % 10)/10) return  0;
+            if(r < 0 && r < (INT_MIN - x % 10)/10) return 0;
+            r = r * 10 + x % 10;
+            x /= 10;
+        }
+        return r;
+    }
+};
