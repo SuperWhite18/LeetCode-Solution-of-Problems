cdafbf387ae411ea7eab3f68f0d9c8e43484d891
superbaiao1208 <1299478311@qq.com>
2021-05-06 23:19:18 +0800

    异或模拟

---
+++ LeetCode_May_clock_in/5_6.1720.解码异或后的数组.cpp
@@ -0,0 +1,17 @@
+//题目链接：
+https://leetcode-cn.com/problems/decode-xored-array/
+
+//代码实现：
+class Solution {
+public:
+    vector<int> decode(vector<int>& encoded, int first) {
+        int n = encoded.size() + 1;
+        vector<int> arr(n);
+        arr[0] = first;
+        for (int i = 1; i < n; i++) {
+            arr[i] = arr[i - 1] ^ encoded[i - 1];
+        }
+        return arr;
+    }
+};
+
